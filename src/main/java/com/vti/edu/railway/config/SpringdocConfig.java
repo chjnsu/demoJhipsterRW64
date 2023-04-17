6b9230bb4d38f71b330452a4e16b11c98ed1d217
nvnam <zp-nguyenn@vng.com.vn>
2023-04-17 18:12:36 +0700

    demo cua xxx ngay 1, tạo thành công

---
+++ src/main/java/com/vti/edu/railway/config/SpringdocConfig.java
@@ -0,0 +1,35 @@
+package com.vti.edu.railway.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@SecurityScheme(
+//    name = "Bearer Authentication",
+//    type = SecuritySchemeType.HTTP,
+//    bearerFormat = "JWT",
+//    scheme = "bearer"
+//)
+public class SpringdocConfig {
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(
+                new Components()
+                    .addSecuritySchemes(
+                        securitySchemeName,
+                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            );
+    }
+}
