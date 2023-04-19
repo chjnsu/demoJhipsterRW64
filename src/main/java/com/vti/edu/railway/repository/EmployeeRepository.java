d2ea630e84aefc3c177fb2ed65f88460e7559be6
nvnam <zp-nguyenn@vng.com.vn>
2023-04-19 21:44:17 +0700

    New Version with full demo

---
+++ src/main/java/com/vti/edu/railway/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.vti.edu.railway.repository;
+
+import com.vti.edu.railway.domain.Employee;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the Employee entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
---
+++ src/main/java/com/vti/edu/railway/repository/RegionRepository.java
@@ -0,0 +1,12 @@
+package com.vti.edu.railway.repository;
+
+import com.vti.edu.railway.domain.Region;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the Region entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Long> {}
